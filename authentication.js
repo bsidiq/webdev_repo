cb0b71533c2f52cfc9134bf248a40c80edd53a14
bsidiq <bsidiq@aol.com>
2025-10-03 18:22:17 -0700

    fetchapi.html added

---
+++ authentication.js
@@ -0,0 +1,10 @@
+// Authentication:
+// helps you ensure if a user has access to a certain resource in backend
+// first time create an account in any website
+// email and pass word goes to backend
+// then from a backend a token (ap!20988dklsh_1345) similar to this will send to the user
+// any time you do any activity in the browswer, the token will be sent along
+// tokens are saved in the browswer or app
+
+
+
